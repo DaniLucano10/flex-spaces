11e9eb35bc1203f87682c83fbf4092456d52680e
DaniLucano10 <olucano10@gmail.com>
2025-05-27 17:39:57 -0500

    Agregando footer al home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero';
+import Footer from './footer/Footer';
+import Services from './services/Services';
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero />
+      <Services />
+      <Footer />
     </div>
   )
 }
