da78d1b0c4f30d0e709ee00723dc9f303d2959bb
DaniLucano10 <olucano10@gmail.com>
2025-05-28 11:11:37 -0500

    Agregando beneficios al home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './Hero/Hero';
 import Footer from './footer/Footer';
 import Services from './services/Services';
+import Benefits from './benefits/Benefits';
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero />
       <Services />
+      <Benefits />
       <Footer />
     </div>
   )
