973964f40819e55124f1778e4018459bab1f517b
DaniLucano10 <olucano10@gmail.com>
2025-05-29 14:04:40 -0500

    Colocando las secciones

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -10,12 +10,7 @@ const Page = () => {
   return (
     <main className="pb-4 pt-16">
       <Banner />
-      {/* Título Secundario */}
-      <section className="w-[85%] mx-auto mt-10 text-center md:text-start">
-        <h2 className="text-xl md:text-3xl text-[#ED8430] font-semibold">
-          Somos más que una plataforma de alquiler de aulas
-        </h2>
-      </section>
+      
       <TextWithImage />
       <Commitment />
       <Social />
