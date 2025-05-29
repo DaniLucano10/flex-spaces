86c6007a19516f3ca9db9e3b99c66320335f6831
DaniLucano10 <olucano10@gmail.com>
2025-05-29 13:03:12 -0500

    Pagina de nosotros

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -1,10 +1,27 @@
-"'use client'"
-const page = () => {
-  return (
-    <div className="text-red-500 p-25">
-      Nosotros
-    </div>
-  )
-}
+"use client";
 
-export default page
+import Banner from "./components/Banner";
+import TextWithImage from "./components/TextWithImage";
+import Commitment from "./components/Commitment";
+import Social from "./components/Social ";
+import ContactForm from "./components/ContactForm";
+
+const Page = () => {
+  return (
+    <main className="pb-4 pt-16">
+      <Banner />
+      {/* Título Secundario */}
+      <section className="w-[85%] mx-auto mt-10 text-center md:text-start">
+        <h2 className="text-xl md:text-3xl text-[#ED8430] font-semibold">
+          Somos más que una plataforma de alquiler de aulas
+        </h2>
+      </section>
+      <TextWithImage />
+      <Commitment />
+      <Social />
+      <ContactForm />
+    </main>
+  );
+};
+
+export default Page;
