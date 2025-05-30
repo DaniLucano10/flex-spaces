a2e8c2fa4bc997344458de0a959a634f464e72a2
DaniLucano10 <olucano10@gmail.com>
2025-05-30 15:55:07 -0500

    modificando

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,11 @@ const nextConfig: NextConfig = {
   experimental: {
     esmExternals: true, // Usa módulos ES modernos para dependencias externas cuando sea posible
   },
-  // Opcional, generalmente es 'web' por defecto, pero puedes asegurar que el target sea moderno
-  webpack(config) {
-    config.target = "web"; 
-    return config;
-  },
+  // // Opcional, generalmente es 'web' por defecto, pero puedes asegurar que el target sea moderno
+  // webpack(config) {
+  //   config.target = "web"; 
+  //   return config;
+  // },
 };
 
 export default nextConfig;
